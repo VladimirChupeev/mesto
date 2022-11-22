af87fe7c150487a91aa23dbc9efdc928142e4448
Chupeev Vladimir <lChupiKl@yandex.ru>
2022-11-23 02:40:03 +0300

    Finish

---
+++ script.js
@@ -0,0 +1,46 @@
+// Кнопка редактирования профиля
+const profileEditButton = document.querySelector(".profile__edit_button");
+// popup профиля
+const profilePopup = document.querySelector("#profile");
+// форма профиля
+const profileForm = document.querySelector(".popup__profile-form");
+// кнопка закрытия формы
+const popupProfileCloseButton = document.querySelector(".popup__close");
+// Верхний инпут
+const nameInputElement = document.querySelector("#name");
+// Нижний инпут
+const hobbyProfileElement = document.querySelector("#hobby");
+// Кнопка добавления картинки
+const addbutton = document.querySelector(".profile__add_button");
+// кнопка сохранить
+const submit = document.querySelector(".popup__submit");
+// строка имени и занятия
+const profileName = document.querySelector(".profile__name");
+const profileHobby = document.querySelector(".profile__hobby");
+
+profileEditButton.addEventListener("click", openProfilePopup);
+popupProfileCloseButton.addEventListener("click", closeProfilePopup);
+profileForm.addEventListener("submit", formSubmitHandler);
+// открытие и закрытие попапа
+function openProfilePopup() {
+  profilePopup.classList.add("popup_opened");
+  fillProfileInputs();
+}
+function closeProfilePopup() {
+  profilePopup.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  const nameInput = nameInputElement.value;
+  const hobbyInput = hobbyProfileElement.value;
+
+  profileName.textContent = nameInput;
+  profileHobby.textContent = hobbyInput;
+  closeProfilePopup();
+}
+
+function fillProfileInputs() {
+    nameInputElement.value = profileName.textContent;
+    hobbyProfileElement.value = profileHobby.textContent;
+}
